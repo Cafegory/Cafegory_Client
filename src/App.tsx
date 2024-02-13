6edddcb36a55b90a6fb79ce4806f1dc36d23d12d
P.S.J <126138033+sojeong0302@users.noreply.github.com>
2024-02-14 00:55:10 +0900

    conf : defaultOptions 설정 #3
    defaultOptions 설정 추가함

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60,
+    },
+  },
+});
 
 function App() {
   return <QueryClientProvider client={queryClient}></QueryClientProvider>;
