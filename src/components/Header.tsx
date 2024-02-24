6e3ccf8bb15a61bfdb98f2457c989bf5986f4b38
sojeong0302 <01duqrl@naver.com>
2024-02-24 22:25:46 +0900

    feat : logo 작업 #5

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import Logo from '../image/logo.jpg';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 10%;
+  flex-direction: row;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 0.5%;
+  align-items: center;
+`;
+
+const LogoImage = styled.img`
+  display: flex;
+  border-radius: 100%;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  width: 53.61px;
+  height: 53.61px;
+`;
+
+const LogoFont = styled.div`
+  display: flex;
+  font-size: 48.2px;
+`;
 
 const Header = () => {
   return (
     <div>
-      <img src={Logo} alt="로고" />
+      <HeaderContainer>
+        <LogoImage src={Logo} alt="로고" />
+        <LogoFont>Cafegory.</LogoFont>
+      </HeaderContainer>
     </div>
   );
 };
