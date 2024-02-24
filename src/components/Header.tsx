06152d28323d947a91f80b61d7ffe63a952f5a54
sojeong0302 <01duqrl@naver.com>
2024-02-25 03:49:34 +0900

    feat: 레이아웃 #5

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,12 @@ import SearchIcon from '../image/searchIcon.png';
 
 const HeaderContainer = styled.div`
   display: flex;
-  height: 107.22px;
+  height: 6.75rem;
   flex-direction: row;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   align-items: center;
   justify-content: space-between;
-  padding: 0em 2em 0em 2em;
+  padding: 0em 2em;
 `;
 
 const LogoDiv = styled.div`
@@ -19,25 +19,26 @@ const LogoDiv = styled.div`
   align-items: center;
 `;
 
-const RightDiv = styled.div`
-  display: flex;
-`;
-
 const LogoImage = styled.img`
   border-radius: 100%;
-  width: 53.61px;
-  height: 53.61px;
+  width: 3.33rem;
+  height: 3.33rem;
   margin-right: 1em;
 `;
 
 const LogoFont = styled.div`
   display: flex;
-  font-size: 37.53px;
+  font-size: 2.34rem;
   font-weight: 500;
 `;
 
+const RightDiv = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 const UserLoggedIn = styled.div`
-  font-size: 21.44px;
+  font-size: 1.35rem;
 `;
 
 const LoginSignupLink = styled.a`
@@ -47,59 +48,60 @@ const LoginSignupLink = styled.a`
 `;
 
 const InputContainer = styled.div`
-  position: relative;
+  margin-left: 5em;
+  border-radius: 8.04px;
+  border: 1px solid #ccc;
+  display: flex;
+  align-items: center;
 `;
 
 const InputField = styled.input`
   padding: 0em 0em 0em 0.5em;
-  border-radius: 8.04px;
-  border: 1px solid #ccc;
-  font-size: 16px;
-  width: 268.05;
-  height: 48.44px;
+  border: none;
+  font-size: 1rem;
+  width: 16.75rem;
+  height: 3.03rem;
+  &:focus {
+    outline: none;
+  }
 `;
 
 const SearchImg = styled.img`
-  position: absolute;
-  right: 10px;
-  top: 50%;
-  transform: translateY(-50%);
-  width: 26.81px;
-  height: 26.81px;
+  width: 1.68rem;
+  height: 1.68rem;
+  cursor: pointer;
 `;
 
 const Header = () => {
   const isLoggined = useStore((state) => state.isLoggined);
 
   return (
-    <div>
-      <HeaderContainer>
-        <LogoDiv>
-          <LogoImage src={Logo} alt="로고" />
-          <LogoFont>Cafegory.</LogoFont>
-        </LogoDiv>
-        <RightDiv>
-          <UserLoggedIn>
-            {' '}
-            {isLoggined ? (
-              <>
-                ooo님 환영합니다!
-                <LoginSignupLink href="">로그아웃</LoginSignupLink>
-              </>
-            ) : (
-              <>
-                <LoginSignupLink href="">로그인</LoginSignupLink>
-                <LoginSignupLink href="">회원가입 </LoginSignupLink>
-              </>
-            )}
-          </UserLoggedIn>
-          <InputContainer>
-            <InputField type="text" placeholder="검색하기" />
-            <SearchImg src={SearchIcon} alt="Search" />
-          </InputContainer>
-        </RightDiv>
-      </HeaderContainer>
-    </div>
+    <HeaderContainer>
+      <LogoDiv>
+        <LogoImage src={Logo} alt="로고" />
+        <LogoFont>Cafegory.</LogoFont>
+      </LogoDiv>
+      <RightDiv>
+        <UserLoggedIn>
+          {' '}
+          {isLoggined ? (
+            <>
+              ooo님 환영합니다!
+              <LoginSignupLink href="">로그아웃</LoginSignupLink>
+            </>
+          ) : (
+            <>
+              <LoginSignupLink href="">회원가입</LoginSignupLink>
+              <LoginSignupLink href="">로그인</LoginSignupLink>
+            </>
+          )}
+        </UserLoggedIn>
+        <InputContainer>
+          <InputField type="text" placeholder="검색하기" />
+          <SearchImg src={SearchIcon} alt="검색 아이콘" />
+        </InputContainer>
+      </RightDiv>
+    </HeaderContainer>
   );
 };
 
