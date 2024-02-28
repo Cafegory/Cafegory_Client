df798b010ac2b64a1f6548cd45f8d9c8c902e0e9
sojeong0302 <01duqrl@naver.com>
2024-02-28 12:47:14 +0900

    fix: Main 페이지 경로 수정 #5
    이전 작업에서 저장 안하고 커밋해서 경로 수정함

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Main from '../pages/Main';
+import Main from '../pages/MainPage/MainPage';
 
 const Router: React.FC = () => {
   return (
