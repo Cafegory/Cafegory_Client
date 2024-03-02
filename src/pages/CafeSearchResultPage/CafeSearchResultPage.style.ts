ed4aa57f1c97237b030871a34d666c30ce378f5e
suyeon <moa0823@naver.com>
2024-03-02 15:52:43 +0900

    design: 카페 검색 결과 타이틀 레이아웃 구현 #15

--- src/pages/CafeSearchResultPage/CafeSearchResultPage.style.ts
+++ src/pages/CafeSearchResultPage/CafeSearchResultPage.style.ts
@@ -7,3 +7,72 @@ export const MainScreen = styled.div`
   display: flex;
   overflow-y: auto;
 `;
+
+export const Container = styled.div`
+  width:83.9%;
+  height:100%;
+  margin-left:16%;
+  margin-top:7%;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+`
+
+export const TitleTextContainer = styled.div`
+  width:100%;
+  height:4rem;
+  display:flex;
+  justify-content:center;
+  border: 0.5px solid transparent; 
+  border-bottom-color: lightgray; 
+  gap:1%;
+`
+
+export const AreaTextFont = styled.div`
+  font-size: 2.3rem;
+  font-weight: 550;
+`
+
+export const ResultTextFont = styled.div`
+  font-size: 2.3rem;
+  font-weight: 360;
+`
+
+export const ResearchContainer = styled.div`
+  display:flex;
+  height:6rem;
+  width:100%;
+  justify-content:center;
+  align-items:center;
+  gap:5%;
+`
+
+export const InputContainer = styled.div`
+  display: flex;
+  border-radius: 6px;
+  border: 1px solid #ccc;
+  width: 30%;
+  height: 50%;
+  justify-content: space-around;
+  background-color: #ffffff;
+  align-items: center;
+`;
+
+export const InputField = styled.input`
+  width: 80%;
+  border: none;
+  font-size: 0.7rem;
+  height: 80%;
+  &:focus {
+    outline: none;
+  }
+  &::placeholder {
+    font-size: 1rem;
+  }
+`;
+
+export const PlaceImg = styled.img`
+  width: 2.3rem;
+  height: 2.3rem;
+  cursor: pointer;
+`;
--- src/pages/CafeSearchResultPage/CafeSearchResultPage.tsx
+++ src/pages/CafeSearchResultPage/CafeSearchResultPage.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
-import { MainScreen } from './CafeSearchResultPage.style';
+import {
+  AreaTextFont,
+  Container,
+  InputContainer,
+  InputField,
+  MainScreen,
+  PlaceImg,
+  ResearchContainer,
+  ResultTextFont,
+  TitleTextContainer,
+} from './CafeSearchResultPage.style';
+import ShortButton from 'components/ShortButton';
 
 const CafeSearchResult: React.FC = () => {
+  const AREA = '역삼동';
+
   return (
     <MainScreen>
+      <Container>
+        <TitleTextContainer>
+          <AreaTextFont>{AREA}</AreaTextFont>
+          <ResultTextFont>기반 카페 검색 결과</ResultTextFont>
+        </TitleTextContainer>
+        <ResearchContainer>
+          <InputContainer>
+            <InputField placeholder={AREA}></InputField>
+            <PlaceImg src="/assets/place-icon.png"></PlaceImg>
+          </InputContainer>
+          <ShortButton message="필터" color="white" />
+          <ShortButton message="검색" color="black" />
+        </ResearchContainer>
+      </Container>
       <Sidebar buttonColors={['white', ,]} />
       <Header />
     </MainScreen>
