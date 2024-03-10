6e26a5eb6d4ff3955c33d7e2ed3451d66ba1e9de
P.S.J <126138033+sojeong0302@users.noreply.github.com>
2024-03-10 19:04:08 +0900

    design: 레이아웃 수정 #27

--- src/pages/CafeMeetingSearchResultPage/CafeMeetingSearchResultPage.style.ts
+++ src/pages/CafeMeetingSearchResultPage/CafeMeetingSearchResultPage.style.ts
@@ -1,23 +1,5 @@
 import styled from 'styled-components';
 
-export const MainScreen = styled.div`
-  background-color: white;
-  min-height: 100vh;
-  width: 100%;
-  display: flex;
-  overflow-y: auto;
-`;
-
-export const Container = styled.div`
-  width: 83.9%;
-  height: 100%;
-  margin-left: 16%;
-  margin-top: 7%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
 export const TitleTextContainer = styled.div`
   width: 100%;
   height: 4rem;
@@ -40,7 +22,7 @@ export const ResultTextFont = styled.div`
 
 export const ResearchContainer = styled.div`
   display: flex;
-  height: 6rem;
+  height: 15rem;
   width: 100%;
   justify-content: center;
   align-items: center;
@@ -52,7 +34,7 @@ export const InputContainer = styled.div`
   border-radius: 6px;
   border: 1px solid #ccc;
   width: 30%;
-  height: 50%;
+  height: 3rem;
   justify-content: space-around;
   background-color: #ffffff;
   align-items: center;
@@ -90,12 +72,6 @@ export const List = styled.div`
   gap: 1.5%;
   border-radius: 0.5rem;
   margin-bottom: 1.5rem;
-`;
-
-export const CafeImg = styled.img`
-  width: 100%;
-  width: 6.5rem;
-  height: 6.5rem;
 `;
 
 export const Detail = styled.div`
@@ -138,8 +114,89 @@ export const MinBeveragePrice = styled.div`
   }
 `;
 
-export const IsOpenImg = styled.img`
-  position: absolute;
-  width: 3rem;
-  height: 2rem;
+export const FitterContainer = styled.div`
+  height: 50rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  gap: 3%;
+  margin-bottom: 5rem;
+`;
+
+export const FitterTitle = styled.div`
+  margin-bottom: 2.5rem;
+  font-size: 2.3rem;
+  font-weight: 550;
+`;
+
+export const ChooseOption = styled.div`
+  width: 60%;
+  height: 70%;
+  display: flex;
+  flex-direction: column;
+  gap: 5%;
+`;
+
+export const StudyAvailability = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ChooseFont = styled.div`
+  font-size: 0.9rem;
+  font-weight: bold;
+`;
+
+export const Choose = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  gap: 1%;
+  align-items: center;
+`;
+
+export const Option = styled.div`
+  display: flex;
+  background-color: rgba(0, 0, 0, 0.05);
+  border-radius: 0.5rem;
+  justify-content: center;
+  width: 13%;
+  height: 70%;
+  font-weight: bold;
+  align-items: center;
+  font-size: 0.9rem;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+
+  &:active {
+    background-color: darkgray;
+  }
+`;
+
+export const MaximumInputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1%;
+  width: 100%;
+  height: 100%;
+`;
+
+export const MaximumInput = styled.input`
+  width: 5%;
+  height: 40%;
+  border: none;
+  &:focus {
+    outline: none;
+  }
+  background-color: rgba(0, 0, 0, 0.05);
+  border-radius: 10px;
+  padding: 1%;
+  font-size: 1.1rem;
 `;
--- src/pages/CafeMeetingSearchResultPage/CafeMeetingSearchResultPage.tsx
+++ src/pages/CafeMeetingSearchResultPage/CafeMeetingSearchResultPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
 import Screen from '../../components/Basic/Screen';
@@ -13,18 +13,84 @@ import {
   TitleTextContainer,
   CafeList,
   List,
-  CafeImg,
   Name,
   Detail,
   Adress,
   BusinessHours,
   MinBeveragePrice,
-  IsOpenImg,
+  FitterContainer,
+  FitterTitle,
+  ChooseOption,
+  StudyAvailability,
+  ChooseFont,
+  Choose,
+  Option,
+  MaximumInputContainer,
+  MaximumInput,
 } from './CafeMeetingSearchResultPage.style';
+
+import {
+  useFilter,
+  updateContent,
+  search,
+  useOption,
+} from './CafeMeetingSearchResultPage.hooks';
+
 import ShortButton from 'components/ShortButton';
 
 const CafeMeetingSearchResultPage: React.FC = () => {
+  const NOT_SPECIFIED = '무관';
+  const CAN_STUDY = '가능';
+  const CANNOT_STUDY = '불가능';
   const AREA = '역삼동';
+
+  const { showFitter, setShowFitter } = useFilter();
+
+  const {
+    onlyJoinAble,
+    setOnlyJoinAble,
+    onlyJoinAbleState,
+    setOnlyJoinAbleState,
+    maxMemberCount,
+    setMaxMemberCount,
+    maxMemberCountState,
+    setMaxMemberCountState,
+    canTalk,
+    setCanTalk,
+    canTalkState,
+    setCanTalkState,
+  } = updateContent();
+
+  const { area, setArea } = search();
+
+  const handleFilterButtonClick = () => {
+    setShowFitter(!showFitter);
+  };
+
+  const ApplyFilter = () => {
+    setShowFitter(false);
+    setOnlyJoinAble(onlyJoinAbleState);
+    setMaxMemberCount(maxMemberCountState);
+    setCanTalk(canTalkState);
+  };
+
+  const {
+    isSelectedOnlyJoinAble,
+    setSelectedOptionOnlyJoinAble,
+    isSelecteCanTalk,
+    setSelectedCanTalk,
+  } = useOption();
+
+  const handleOnlyJoinAbleOptionClick = (value: 'TRUE' | 'FALSE') => {
+    setSelectedOptionOnlyJoinAble(value);
+  };
+
+  const handleCanTalkOptionClick = (value: 'YES' | 'NO' | 'BOTH') => {
+    setSelectedCanTalk(value);
+  };
+
+  useEffect(() => {}, [onlyJoinAble, maxMemberCount, canTalk, area]);
+
   const API: any = [
     {
       cafeId: 1,
@@ -71,7 +137,125 @@ const CafeMeetingSearchResultPage: React.FC = () => {
           <AreaTextFont>{AREA}</AreaTextFont>
           <ResultTextFont>기반 카공 모임 검색 결과</ResultTextFont>
         </TitleTextContainer>
-        <ResearchContainer></ResearchContainer>
+        <ResearchContainer>
+          <InputContainer>
+            <InputField
+              placeholder={AREA}
+              value={area}
+              onChange={(e) => setArea(e.target.value)}
+            ></InputField>
+            <PlaceImg src="/assets/place-icon.png"></PlaceImg>
+          </InputContainer>
+          <ShortButton
+            message="필터"
+            color="white"
+            onClick={handleFilterButtonClick}
+          />
+          <ShortButton message="검색" color="black" />
+        </ResearchContainer>
+        {showFitter && (
+          <FitterContainer>
+            <FitterTitle>필터</FitterTitle>
+            <ChooseOption>
+              <StudyAvailability>
+                <ChooseFont>카공 참여 가능?</ChooseFont>
+                <Choose>
+                  <Option
+                    onClick={() => {
+                      handleOnlyJoinAbleOptionClick('TRUE');
+                      setOnlyJoinAbleState(true);
+                    }}
+                    style={{
+                      backgroundColor:
+                        isSelectedOnlyJoinAble === 'TRUE'
+                          ? 'rgba(0, 0, 0, 0.2)'
+                          : 'rgba(0, 0, 0, 0.05)',
+                    }}
+                  >
+                    {CAN_STUDY}
+                  </Option>
+                  <Option
+                    onClick={() => {
+                      handleOnlyJoinAbleOptionClick('FALSE');
+                      setOnlyJoinAbleState(false);
+                    }}
+                    style={{
+                      backgroundColor:
+                        isSelectedOnlyJoinAble === 'FALSE'
+                          ? 'rgba(0, 0, 0, 0.2)'
+                          : 'rgba(0, 0, 0, 0.05)',
+                    }}
+                  >
+                    {CANNOT_STUDY}
+                  </Option>
+                </Choose>
+              </StudyAvailability>
+              <StudyAvailability>
+                <ChooseFont>카공 최대 인원</ChooseFont>
+                <MaximumInputContainer>
+                  <MaximumInput
+                    type="number"
+                    defaultValue={0}
+                    min={0}
+                    max={10}
+                    onChange={(e) =>
+                      setMaxMemberCountState(parseInt(e.target.value))
+                    }
+                  />
+                  명
+                </MaximumInputContainer>
+              </StudyAvailability>
+              <StudyAvailability>
+                <ChooseFont>대화 가능?</ChooseFont>
+                <Choose>
+                  <Option
+                    onClick={() => {
+                      handleCanTalkOptionClick('YES');
+                      setCanTalkState('YES');
+                    }}
+                    style={{
+                      backgroundColor:
+                        isSelecteCanTalk === 'YES'
+                          ? 'rgba(0, 0, 0, 0.2)'
+                          : 'rgba(0, 0, 0, 0.05)',
+                    }}
+                  >
+                    가능
+                  </Option>
+                  <Option
+                    onClick={() => {
+                      handleCanTalkOptionClick('NO');
+                      setCanTalkState('NO');
+                    }}
+                    style={{
+                      backgroundColor:
+                        isSelecteCanTalk === 'NO'
+                          ? 'rgba(0, 0, 0, 0.2)'
+                          : 'rgba(0, 0, 0, 0.05)',
+                    }}
+                  >
+                    불가능
+                  </Option>
+                  <Option
+                    onClick={() => {
+                      handleCanTalkOptionClick('BOTH');
+                      setCanTalkState('BOTH');
+                    }}
+                    style={{
+                      backgroundColor:
+                        isSelecteCanTalk === 'BOTH'
+                          ? 'rgba(0, 0, 0, 0.2)'
+                          : 'rgba(0, 0, 0, 0.05)',
+                    }}
+                  >
+                    무관
+                  </Option>
+                </Choose>
+              </StudyAvailability>
+            </ChooseOption>
+            <ShortButton message="적용" color="black" onClick={ApplyFilter} />
+          </FitterContainer>
+        )}
         <CafeList>
           {API.map((study) => (
             <List>
@@ -85,7 +269,7 @@ const CafeMeetingSearchResultPage: React.FC = () => {
           ))}
         </CafeList>
       </Container>
-      <Sidebar buttonColors={['white', ,]} />
+      <Sidebar buttonColors={[, 'white', ,]} />
       <Header />
     </Screen>
   );
