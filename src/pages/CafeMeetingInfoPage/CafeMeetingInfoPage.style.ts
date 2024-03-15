bd1e8f9c17c8ebce5e9e496b0d9bdeb86f521921
suyeon <moa0823@naver.com>
2024-03-15 15:58:29 +0900

    design: 카공 모음 상세 레이아웃#33

--- src/pages/CafeMeetingInfoPage/CafeMeetingInfoPage.style.ts
+++ src/pages/CafeMeetingInfoPage/CafeMeetingInfoPage.style.ts
@@ -1,8 +1,134 @@
 import styled from 'styled-components';
 
-export const CafeInfoContainer = styled.div`
+export const CafeMeetingInfoContainer = styled.div`
     width:85%;
     display:flex;
     flex-direction:column;
     gap:3rem;
+    padding-bottom:5rem;
 `;
+
+export const MeetingNameContainer = styled.div`
+  width:100%;
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  gap:0.3rem;
+`;
+
+export const MeetingNameFont = styled.div`
+  font-size:1.55rem;
+  font-weight:550;
+  width:100%
+`;
+
+export const AddressFont = styled.div`
+  font-size:1rem;
+  width:100%;
+  color:gray;
+`
+
+export const TitleContainer = styled.div`
+  display:flex;
+  flex-direction:column;
+  gap:1rem;
+`
+
+export const TitleFont = styled.div`
+  font-size:1.6rem;
+  font-weight:550;
+  width:100%;
+`
+
+export const MemberCountContainer = styled.div`
+  display:flex;
+  align-items:center;
+  gap:1rem;
+`
+
+export const CanJoinMemberCount = styled.div`
+  font-size:1.2rem;
+  font-weight:550;
+  color:black;
+  width:fit-content;
+`
+
+export const CanJoinFont = styled.div`
+  font-size:1.2rem;
+  font-weight:550;
+  color:#52FF00;
+  background-color:black;
+  width:fit-content;
+  padding:0.2rem 0.5rem 0.2rem 0.5rem;
+  border-radius:10px;
+`
+
+export const CannotJoinMemberCount = styled.div`
+  font-size:1.2rem;
+  font-weight:550;
+  color:red;
+  width:fit-content;
+`
+
+export const CannotJoinFont = styled.div`
+  font-size:1.2rem;
+  font-weight:550;
+  color:red;
+  background-color:black;
+  width:fit-content;
+  padding:0.2rem 0.5rem 0.2rem 0.5rem;
+  border-radius:10px;
+`
+
+export const GrayFont = styled.div`
+  width:100%;
+  color:gray;
+  font-size:1.2rem;
+`
+
+export const ButtonContainer = styled.div`
+  width:100%;
+  display:flex;
+  flex-direction:column;
+  gap:1rem;
+`
+
+export const QuestionBoxContainer = styled.div`
+  width:100%;
+  display:flex;
+  flex-direction:column;
+  gap:0.4rem;
+`
+export const QuestionBox = styled.div`
+  width:100%;
+  display:flex;
+  flex-direction:column;
+  gap:0.5rem;
+`
+
+export const QuestionBoxUser = styled.div`
+  width:100%;
+  display:flex;
+  align-items:center;
+  gap:1rem;
+`
+
+export const ProfileImg = styled.img`
+  width: 1.2rem;
+  height: 1.2rem;
+`;
+
+export const UserNameFont = styled.div`
+  color:gray;
+  font-size:1.2rem;
+`
+export const QuestionContentFont = styled.div`
+  font-size:1.1rem;
+`
+
+export const ReplyBox = styled.div`
+  font-size:1.1rem;
+  display:flex;
+  gap:1rem;
+`
--- src/pages/CafeMeetingInfoPage/CafeMeetingInfoPage.tsx
+++ src/pages/CafeMeetingInfoPage/CafeMeetingInfoPage.tsx
@@ -3,11 +3,167 @@ import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
 import Screen from 'components/Basic/Screen';
 import Container from 'components/Basic/Container';
+import {
+  CafeMeetingInfoContainer,
+  MeetingNameContainer,
+  MeetingNameFont,
+  AddressFont,
+  TitleFont,
+  CanJoinFont,
+  CanJoinMemberCount,
+  MemberCountContainer,
+  TitleContainer,
+  CannotJoinFont,
+  CannotJoinMemberCount,
+  GrayFont,
+  ButtonContainer,
+  QuestionBoxContainer,
+  QuestionBox,
+  ProfileImg,
+  QuestionBoxUser,
+  UserNameFont,
+  QuestionContentFont,
+  ReplyBox,
+} from './CafeMeetingInfoPage.style';
+import LongButton from 'components/LongButton';
 
 const CafeMeetingInfo: React.FC = () => {
+  const api = {
+    cafeId: 1,
+    id: 1,
+    name: '알아서 공부하자',
+    startDateTime: 'yyyy-MM-ddThh:mm:ss',
+    endDateTime: 'yyyy-MM-ddThh:mm:ss',
+    maxMemberCount: 7,
+    nowMemberCount: 3,
+    canTalk: true,
+    canJoin: true,
+    isEnd: false,
+  };
+
+  const qnaApi = {
+    replyWriter: {
+      memberId: 2,
+    },
+    questions: [
+      {
+        questionWriter: {
+          memberId: 1,
+          name: '취준생',
+          thumbnailImg: 'https://~~',
+        },
+        questionInfo: {
+          questionId: 1,
+          content: '조금 늦어도 될까요?',
+        },
+        reply: {
+          replyId: 1,
+          content: '아니요! 웬만하면 정시에 시작하려고 합니다',
+        },
+      },
+      {
+        questionWriter: {
+          memberId: 1,
+          name: '취준생',
+          thumbnailImg: 'https://~~',
+        },
+        questionInfo: {
+          questionId: 1,
+          content: '조금 늦어도 될까요?',
+        },
+        reply: {
+          replyId: 1,
+          content: '아니요! 웬만하면 정시에 시작하려고 합니다',
+        },
+      },
+    ],
+  };
+
+  const hi = () => {};
+
   return (
     <Screen>
-      <Container></Container>
+      <Container>
+        <CafeMeetingInfoContainer>
+          <MeetingNameContainer>
+            <MeetingNameFont>{api.name}</MeetingNameFont>
+            <AddressFont>스타벅스 강남R점</AddressFont>
+          </MeetingNameContainer>
+          <TitleContainer>
+            <TitleFont>모집 현황</TitleFont>
+            {api.canJoin ? (
+              <MemberCountContainer>
+                <CanJoinMemberCount>
+                  {api.nowMemberCount}명 / {api.maxMemberCount}명
+                </CanJoinMemberCount>
+                <CanJoinFont>모집중</CanJoinFont>
+              </MemberCountContainer>
+            ) : (
+              <MemberCountContainer>
+                <CannotJoinMemberCount>
+                  {api.nowMemberCount}명 / {api.maxMemberCount}명
+                </CannotJoinMemberCount>
+                <CannotJoinFont>모집 마감</CannotJoinFont>
+              </MemberCountContainer>
+            )}
+          </TitleContainer>
+          <TitleContainer>
+            <TitleFont>시간</TitleFont>
+            <GrayFont>
+              {api.startDateTime}~{api.endDateTime}
+            </GrayFont>
+          </TitleContainer>
+          <TitleContainer>
+            <TitleFont>구성원 간 소통 여부</TitleFont>
+            {api.canTalk ? (
+              <GrayFont>가능</GrayFont>
+            ) : (
+              <GrayFont>불가능</GrayFont>
+            )}
+          </TitleContainer>
+          <TitleContainer>
+            <TitleFont>오픈 채팅방 주소</TitleFont>
+            <GrayFont>오픈 채팅방 주소</GrayFont>
+          </TitleContainer>
+          <ButtonContainer>
+            <LongButton
+              message="그룹 정보 수정"
+              onClick={hi}
+              color="black"
+            ></LongButton>
+            <LongButton
+              message="모집 마감"
+              onClick={hi}
+              color="red"
+            ></LongButton>
+          </ButtonContainer>
+          <TitleContainer>
+            <TitleFont>QnA</TitleFont>
+            {qnaApi.questions.map((question, index) => (
+              <QuestionBoxContainer>
+                <QuestionBox>
+                  <QuestionBoxUser>
+                    <ProfileImg
+                      src={qnaApi.questions[index].questionWriter.thumbnailImg}
+                      alt="프로필 사진"
+                    ></ProfileImg>
+                    <UserNameFont>
+                      {qnaApi.questions[index].questionWriter.name}
+                    </UserNameFont>
+                  </QuestionBoxUser>
+                  <QuestionContentFont>
+                    {qnaApi.questions[index].questionInfo.content}
+                  </QuestionContentFont>
+                </QuestionBox>
+                <ReplyBox>
+                  <div>↳</div>
+                  <div>{qnaApi.questions[index].reply.content}</div>
+                </ReplyBox>
+              </QuestionBoxContainer>
+            ))}
+          </TitleContainer>
+        </CafeMeetingInfoContainer>
+      </Container>
       <Sidebar buttonColors={[, 'white']} />
       <Header />
     </Screen>
