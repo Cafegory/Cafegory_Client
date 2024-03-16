6fb7bb5c6e472021b38af9ac2da74b8a3f8b0b66
suyeon <moa0823@naver.com>
2024-03-16 15:39:55 +0900

    design: 마이페이지 레이아웃 #36

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -3,11 +3,33 @@ import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
 import Screen from 'components/Basic/Screen';
 import Container from 'components/Basic/Container';
+import {
+  IntroduceFont,
+  MypageContainer,
+  NameFont,
+  ProfileImg,
+  TitleFont,
+} from './MyPage.style';
+import ShortButton from 'components/ShortButton';
 
 const My: React.FC = () => {
+  const api = {
+    name: '취준생',
+    thumbnailImg: 'https://~~',
+    introduction: '안녕하세요 수연이에요 ',
+  };
+
   return (
     <Screen>
-      <Container></Container>
+      <Container>
+        <MypageContainer>
+          <TitleFont>마이페이지</TitleFont>
+          <ProfileImg src={api.thumbnailImg} alt="프로필 사진"></ProfileImg>
+          <NameFont>{api.name}</NameFont>
+          <IntroduceFont>{api.introduction}</IntroduceFont>
+          <ShortButton message="수정하기" color="black"></ShortButton>
+        </MypageContainer>
+      </Container>
       <Sidebar buttonColors={[, , 'white']} />
       <Header />
     </Screen>
