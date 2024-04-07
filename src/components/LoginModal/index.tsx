af29d66a40d7f4c5ae5539cde0a2dab7de87734b
P.S.J <126138033+sojeong0302@users.noreply.github.com>
2024-04-07 23:33:25 +0900

    feat: 카카오 소셜 로그인 api 연동함 #39

--- src/components/LoginModal/index.tsx
+++ src/components/LoginModal/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   LoginModal,
   ModalBackdrop,
@@ -12,25 +12,30 @@ import { useStore } from './LoginModal.hooks';
 
 const Login: React.FC = () => {
   const toggleLoginModal = useStore((state) => state.toggleLoginModal);
+
   const closeModal = () => {
     toggleLoginModal();
   };
 
-  const KAKAO_REST_API_KEY = 'dea99a8b573d3e7052b32772749fd70a';
-  const KAKAO_REDIRECT_URI = 'http://52.78.210.204/';
-  const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}/oauth2/kakao&response_type=code`;
-
-  const kakaoLoginHandler = () => {
-    window.location.href = KAKAO_URL;
-  };
-
+  // const NAVER_REST_API_KEY = 'xCfIxbbRWciauIoFEjvc';
+  // const NAVER_STATE = false;
+  // const NAVER_REDIRECT_URI = 'http://localhost:3000/';
+  // const NAVER_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=lCu2oE140nmA93yKzwjb&state=HJ8nwTnQ7h&redirect_uri=http://{서버 주소}/oauth2/naver`;
   const NAVER_REST_API_KEY = 'xCfIxbbRWciauIoFEjvc';
   const NAVER_STATE = false;
   const NAVER_REDIRECT_URI = 'http://localhost:3000/';
-  const NAVER_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_REST_API_KEY}&state=${NAVER_STATE}&redirect_uri=${NAVER_REDIRECT_URI}`;
+  const NAVER_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=lCu2oE140nmA93yKzwjb&state=HJ8nwTnQ7h&redirect_uri=${NAVER_REDIRECT_URI}`;
 
   const naverLoginHandler = () => {
     window.location.href = NAVER_URL;
+  };
+
+  const KAKAO_REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+  const KAKAO_REDIRECT_URI = 'http://localhost:3000/';
+  const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+
+  const kakaoLoginHandler = () => {
+    window.location.href = KAKAO_URL;
   };
 
   return (
