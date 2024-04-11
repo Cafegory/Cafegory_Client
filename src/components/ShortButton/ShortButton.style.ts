bb2429f67ed3b4652c0a38d56e19003f6299c970
suyeon <moa0823@naver.com>
2024-04-11 15:26:42 +0900

    feat: 컴포넌트 공통 작업 #48

--- src/components/ShortButton/ShortButton.style.ts
+++ src/components/ShortButton/ShortButton.style.ts
@@ -13,15 +13,23 @@ export const ButtonStyle = styled.div<ShortButtonProps>`
   transition:
     background-color 0.3s,
     color 0.3s;
-  &:hover {
-    background-color: ${(props) =>
-      props.color === 'white' ? '#e0e0e0' : 'rgba(0, 0, 0, 0.8)'};
-    color: ${(props) =>
-      props.color === 'white' ? 'rgba(0, 0, 0, 0.8)' : 'white'};
-  }
   background-color: ${(props) => (props.color === 'black' ? 'black' : 'white')};
   &:active {
     background-color: darkgray;
+  }
+
+  @media (hover: hover) and (pointer: fine) {
+    &:hover {
+      background-color: ${(props) =>
+        props.color === 'white' ? '#e0e0e0' : 'rgba(0, 0, 0, 0.8)'};
+      color: ${(props) =>
+        props.color === 'white' ? 'rgba(0, 0, 0, 0.8)' : 'white'};
+    }
+  }
+
+  @media (max-width: 768px) {
+    height: 2.5rem;
+    width: 40%;
   }
 `;
 
