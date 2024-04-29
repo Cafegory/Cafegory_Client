42fe70cc2d4a7bf4ad48f5674fa12fd0fc9fff04
suyeon <moa0823@naver.com>
2024-04-29 15:01:29 +0900

    design:토큰 만료 모달 레이아웃 #91

--- src/components/RefreashModal/RefreashModal.style.ts
+++ src/components/RefreashModal/RefreashModal.style.ts
@@ -13,7 +13,7 @@ export const RefreashModal = styled.div`
   justify-content: center;
   flex-direction: column;
   border-radius: 20px;
-  gap: 1rem;
+  gap: 0.7rem;
 
   @media screen and (max-width: 768px) {
     width:90%;
@@ -28,3 +28,26 @@ export const ModalBackdrop = styled.div`
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
 `;
+
+export const MessageFont = styled.div`
+  width:100%;
+  font-size:1.1rem;
+  display:flex;
+  justify-content:center;
+  color:red;
+`
+
+export const QuestionFont = styled.div`
+  width:100%;
+  font-size:1.3rem;
+  display:flex;
+  justify-content:center;
+  font-weight:440;
+`
+
+export const ButtonContainer = styled.div`
+  display:flex;
+  justify-content:center;
+  gap:1rem;
+  width:100%;
+`
--- src/components/RefreashModal/index.tsx
+++ src/components/RefreashModal/index.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
-import { RefreashModal, ModalBackdrop } from './RefreashModal.style';
+import {
+  RefreashModal,
+  ModalBackdrop,
+  ButtonContainer,
+  MessageFont,
+  QuestionFont,
+} from './RefreashModal.style';
 import { refreashStore } from './RefreashModal.hooks';
+import ShortButton from 'components/ShortButton';
+import { useNavigate } from 'react-router-dom';
 
 const Refreash: React.FC = () => {
   const toggleRefreashModal = refreashStore(
     (state) => state.toggleRefreashModal,
   );
 
+  const navigate = useNavigate();
+
   return (
     <>
-      <RefreashModal></RefreashModal>
+      <RefreashModal>
+        <MessageFont>토큰이 만료되었습니다.</MessageFont>
+        <QuestionFont>로그인 상태를 유지하시겠습니까?</QuestionFont>
+        <ButtonContainer>
+          <ShortButton
+            message="유지"
+            color="white"
+            onClick={() => {
+              navigate('/main');
+            }}
+          ></ShortButton>
+          <ShortButton
+            message="로그아웃"
+            color="black"
+            onClick={() => {
+              navigate('/main');
+            }}
+          ></ShortButton>
+        </ButtonContainer>
+      </RefreashModal>
       <ModalBackdrop></ModalBackdrop>
     </>
   );
