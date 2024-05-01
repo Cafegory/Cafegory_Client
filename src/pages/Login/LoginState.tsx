316bd6db5e397567316fdd0ed09144bc40746ce4
suyeon <moa0823@naver.com>
2024-05-02 00:32:04 +0900

    d

--- src/pages/Login/LoginState.tsx
+++ src/pages/Login/LoginState.tsx
@@ -11,9 +11,7 @@ const LoginState: React.FC = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          `http://52.78.210.204/oauth2/kakao?code=${code}`,
-        );
+        const response = await axios.get(`/oauth2/kakao?code=${code}`);
         console.log('hi');
         console.log(response);
         console.log(response.data);
