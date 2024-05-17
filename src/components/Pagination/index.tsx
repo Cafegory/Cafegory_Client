42fc2a9ed6da2adb2e90cccc9be9d98dc8bf4f3d
suyeon <moa0823@naver.com>
2024-05-17 17:02:17 +0900

    feat: 페이지가 많아지는 경우 추가 기능 #162

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -15,8 +15,28 @@ const Pagination: React.FC<PaginationProps> = ({
     onPageChange(page);
   };
 
+  const elementsOfViewPage = 5;
+
+  const [viewStartPage, setViewStartPage] = React.useState(0);
+  const [viewLastPage, setViewLastPage] = React.useState(5);
+
+  const navigateNextViewPage = () => {
+    setViewStartPage((start) => start + elementsOfViewPage);
+    setViewLastPage((last) => last + elementsOfViewPage);
+    onPageChange(viewStartPage + elementsOfViewPage + 1);
+  };
+
+  const navigatePrevViewPage = () => {
+    setViewStartPage((start) => start - elementsOfViewPage);
+    setViewLastPage((last) => last - elementsOfViewPage);
+    onPageChange(viewStartPage - elementsOfViewPage + 1);
+  };
+
   return (
     <PageContainer>
+      {viewStartPage > 0 && (
+        <PageButton onClick={navigatePrevViewPage}>&lt;</PageButton>
+      )}
       {Array.from(Array(totalPage), (_, index) => {
         if (currentPage === index + 1) {
           return <CurrentPageButton key={index}>{index + 1}</CurrentPageButton>;
@@ -27,7 +47,10 @@ const Pagination: React.FC<PaginationProps> = ({
             </PageButton>
           );
         }
-      })}
+      }).slice(viewStartPage, Math.min(viewLastPage, totalPage))}
+      {totalPage > viewLastPage && (
+        <PageButton onClick={navigateNextViewPage}>&gt;</PageButton>
+      )}
     </PageContainer>
   );
 };
